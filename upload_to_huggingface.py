eec688b2c4f8523488bbf57c7f13d2909850ea77
alexds9 <alexds9@gmail.com>
2023-05-01 18:55:34 +0300

    1. Add progress indication for upload to huggingface.

--- upload_to_huggingface.py
+++ upload_to_huggingface.py
@@ -2,6 +2,22 @@ import argparse
 import hashlib
 import os
 from huggingface_hub import HfApi
+from tqdm import tqdm
+
+class TqdmFileReader:
+    def __init__(self, file):
+        self.file = file
+        self.progress_bar = tqdm(total=os.path.getsize(file.name), unit="B", unit_scale=True)
+
+    def read(self, size=-1):
+        data = self.file.read(size)
+        self.progress_bar.update(len(data))
+        return data
+
+    def close(self):
+        self.progress_bar.close()
+        self.file.close()
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Upload files to Hugging Face")
@@ -32,14 +48,18 @@ def upload_files(args):
             try:
                 path_in_repo = os.path.join(args.path, filename)  # Use os.path.join to avoid double slashes
                 print(f"Attempt {attempt}: Uploading to HF: huggingface.co/{repo_id}/{path_in_repo}, sha256: {readable_hash}")
-                response = api.upload_file(
-                    path_or_fileobj=filepath,
-                    path_in_repo=path_in_repo,
-                    repo_id=repo_id,
-                    repo_type=None,
-                    token=token,
-                    create_pr=1,
-                )
+
+                with open(ckpt, "rb") as file:
+                    tqdm_file = TqdmFileReader(file)
+
+                    response = api.upload_file(
+                        path_or_fileobj=tqdm_file,
+                        path_in_repo=path_in_repo,
+                        repo_id=repo_id,
+                        repo_type=None,
+                        token=token,
+                        create_pr=1,
+                    )
                 print(response)
                 print(f"Upload successful for {filename}")
                 break
