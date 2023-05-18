3c88a4bd298eccfcb8d2e63daf7d66ad1b27db2c
alexds9 <alexds9@gmail.com>
2023-05-18 09:29:08 +0300

    1. Count and report removed EXIFs.

--- images_find_bad_exif.py
+++ images_find_bad_exif.py
@@ -15,6 +15,7 @@ def check_image_integrity(file_path, is_remove_exif):
     # file_extension = os.path.splitext(file_name)[1].lower()
     is_valid = False
     is_failed_to_read_exif = False
+    is_exif_removed = False
 
     try:
         img = Image.open(open(file_path, 'rb'))
@@ -50,11 +51,12 @@ def check_image_integrity(file_path, is_remove_exif):
             image_without_exif.close()
 
             is_valid = True
+            is_exif_removed = True
 
         except Exception as e:
             print(f"Error occurred while trying to remove exif for: {file_name}. Error: {str(e)}, {traceback.format_exc()}")
 
-    return is_valid
+    return is_valid, is_exif_removed
 
 if __name__ == "__main__":
     
@@ -74,6 +76,7 @@ if __name__ == "__main__":
     total = len(os.listdir(folder_path))
     count_valid = 0
     count_invalid = 0
+    count_exif_removed = 0
 
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
@@ -81,11 +84,15 @@ if __name__ == "__main__":
             count_all += 1
             file_is_image = file_name.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'))
             if file_is_image:
-                if True == check_image_integrity( file_path, is_remove_exif ):
-                    count_valid += 1
-                else:
-                    count_invalid += 1
+                is_valid, is_exif_removed = check_image_integrity( file_path, is_remove_exif )
 
+                if is_exif_removed == is_valid:
+                    count_invalid += 1
+                else:
+                    count_valid += 1
+
+                if is_exif_removed:
+                    count_exif_removed += 1
                 print(f'\rProcessed {count_all}/{total} images.', end='', flush=True)
 
-    print(f'\rDone, valid: {count_valid} images, invalid: {count_invalid} images.', end='', flush=True)
+    print(f'\rDone, valid: {count_valid} images, invalid: {count_invalid} images, removed exif: {count_exif_removed}.', end='', flush=True)
