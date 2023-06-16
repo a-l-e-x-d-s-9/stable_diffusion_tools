fef39db79f8501c793f044087ebcd5709f87126a
alexds9 <alexds9@gmail.com>
2023-06-16 13:54:34 +0300

    1. Support multiple files upload at same time.

--- huggingface_upload.py
+++ huggingface_upload.py
@@ -2,33 +2,13 @@ import argparse
 import hashlib
 import os
 import json
+import logging
 from huggingface_hub import HfApi
 from tqdm import tqdm
-import io
 import concurrent.futures
+import time
+import threading
 
-
-class TqdmFileReader(io.BufferedIOBase):
-    def __init__(self, file):
-        super().__init__()
-        self.file = file
-        self.progress_bar = tqdm(total=os.path.getsize(file.name), unit="B", unit_scale=True)
-
-    def read(self, size=-1):
-        data = self.file.read(size)
-        self.progress_bar.update(len(data))
-        return data
-
-    def seek(self, offset, whence=io.SEEK_SET):
-        result = self.file.seek(offset, whence)
-        return result
-
-    def tell(self):
-        return self.file.tell()
-
-    def close(self):
-        self.progress_bar.close()
-        self.file.close()
 
 
 def get_args():
@@ -40,53 +20,62 @@ def get_args():
     parser.add_argument("--remove", action="store_true", help="Remove files after upload")
     return parser.parse_args()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def upload_file(filepath, base_directory, repo_id, token, api, max_attempts=3, remove=False):
+def compute_sha256(filepath, chunk_size=8192):
+    """Compute the sha256 hash of a file."""
+    hash_sha256 = hashlib.sha256()
+    with open(filepath, "rb") as f:
+        for chunk in iter(lambda: f.read(chunk_size), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
+
+def upload_file(filepath, base_directory, repo_id, token, api, progress_bar, progress_bar_lock, max_attempts=3, remove=False):
     filename = os.path.basename(filepath)
     readable_hash = ""
 
     # Compute the path in the repository by removing the base directory
     path_in_repo = os.path.relpath(filepath, base_directory)
 
-    with open(filepath, "rb") as f:
-        bytes = f.read()
-        readable_hash = hashlib.sha256(bytes).hexdigest()
+    readable_hash = compute_sha256(filepath)
 
     for attempt in range(1, max_attempts + 1):
         try:
-            print(
+            logger.info(
                 f"Attempt {attempt}: Uploading to HF: huggingface.co/{repo_id}/{path_in_repo}, sha256: {readable_hash}")
 
             with open(filepath, "rb") as file:
-                tqdm_file = TqdmFileReader(file)
-
-
                 response = api.upload_file(
-                    path_or_fileobj=tqdm_file,
+                    path_or_fileobj=file,
                     path_in_repo=path_in_repo,
                     repo_id=repo_id,
                     repo_type=None,
                     token=token,
                     create_pr=True,
                 )
-            print(response)
+
+            # Update progress bar
+            with progress_bar_lock:
+                progress_bar.update(os.path.getsize(filepath))
 
             # Check if the upload was successful
             if response and isinstance(response, str) and response.startswith("https://"):
-                print(f"File {filepath} uploaded successfully.")
+                logger.info(f"File {filepath} uploaded successfully.")
 
                 # If remove flag is set, remove the file locally
                 if remove:
                     os.remove(filepath)
-                    print(f"File {filepath} deleted locally.")
+                    logger.info(f"File {filepath} deleted locally.")
             else:
-                print(f"Failed to upload file {filepath}.")
+                logger.info(f"Failed to upload file {filepath}.")
 
             break
         except Exception as e:
-            print(f"Error uploading {filename}: {e}")
+            logger.info(f"Error uploading {filename}: {e}")
             if attempt == max_attempts:
-                print(f"Failed to upload {filename} after {max_attempts} attempts")
+                logger.info(f"Failed to upload {filename} after {max_attempts} attempts")
+
 
 def upload_files(args):
     # Read configurations
@@ -102,17 +91,29 @@ def upload_files(args):
     api = HfApi()
     repo_id = args.repository
 
+
+    # Calculate total size of all files
+    total_size = sum(os.path.getsize(filepath) for filepath in file_list)
+
+    # Create a progress bar
+    progress_bar = tqdm(total=total_size, unit="GB", unit_scale=True)
+
+    # Create a lock for progress bar
+    progress_bar_lock = threading.Lock()
+
     # Use ThreadPoolExecutor to parallelize the uploads
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         futures = [
-            executor.submit(upload_file, filepath, base_directory, repo_id, token, api, remove=args.remove)
+            # pass the progress_bar to the upload_file function
+            executor.submit(upload_file, filepath, base_directory, repo_id, token, api, progress_bar,
+                            progress_bar_lock, remove=args.remove)
             for filepath in file_list
         ]
         for future in concurrent.futures.as_completed(futures):
             future.result()
 
-    print("DONE")
-    print("Go to your repo and accept the PRs this created to see your files")
+    logger.info("DONE")
+    logger.info("Go to your repo and accept the PRs this created to see your files")
 
 
 def main():
