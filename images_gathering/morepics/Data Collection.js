239d0fc1f715b1830d720355829396a0580f7f54
alexds9 <alexds9@gmail.com>
2023-06-25 15:13:03 +0300

    1. More download options.

---
+++ images_gathering/morepics/Data Collection.js
@@ -0,0 +1,116 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.pornpics.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pornpics.com
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+// ==UserScript==
+// @name         Image Info Extractor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Extract image info from HTML
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var currentUrl = window.location.href;
+    var storedData = JSON.parse(GM_getValue("storedData", "{}"));
+    var autoStoreData = GM_getValue("autoStoreData", false);
+
+    // Create a fixed counter element
+    var counter = document.createElement('div');
+    counter.style.position = 'fixed';
+    counter.style.top = '110px';
+    counter.style.left = '10px';
+    counter.style.fontSize = '55px';
+    counter.style.color = 'red';
+    counter.style.zIndex = '9999'; // ensures counter is always on top
+    counter.innerHTML = Object.keys(storedData).length;
+    document.body.appendChild(counter);
+
+    function updateCounter() {
+        counter.innerHTML = Object.keys(storedData).length;
+    }
+
+    function removeDataFromList(imageUrl) {
+        delete storedData[imageUrl];
+        GM_setValue("storedData", JSON.stringify(storedData));
+        updateCounter();
+    }
+
+    function addDataToList(url, data) {
+        if (!storedData[url]) {
+            storedData[url] = data;
+            GM_setValue("storedData", JSON.stringify(storedData));
+            updateCounter();
+        }
+    }
+
+
+    async function copyToClipboard() {
+        try {
+            let dataStrings = Object.values(storedData).map(data => JSON.stringify(data, null, 2));
+            await navigator.clipboard.writeText(dataStrings.join('\n'));
+        } catch (err) {
+            console.error('Failed to copy data: ', err);
+        }
+    }
+
+    function clearList() {
+        storedData = {};
+        GM_setValue("storedData", JSON.stringify(storedData));
+        updateCounter();
+    }
+
+    GM_registerMenuCommand('Copy data to Clipboard', copyToClipboard);
+    GM_registerMenuCommand('Clear data', clearList);
+
+    let imageLinks = [];
+    let modelName = '';
+    let tags = [];
+
+    // Collect all href from li elements with class 'thumbwook'
+    let thumbwookElems = document.querySelectorAll("li.thumbwook a.rel-link");
+    thumbwookElems.forEach(function (elem) {
+        imageLinks.push(elem.href);
+    });
+
+    // Check for Model title and Extract model name from div with class 'gallery-info__content'
+    let modelTitleElems = document.querySelectorAll(".gallery-info__item span.gallery-info__title");
+    modelTitleElems.forEach(function(modelTitleElem) {
+        if (modelTitleElem.innerText.trim() === "Models:") {
+            let modelNameElem = modelTitleElem.parentElement.querySelector(".gallery-info__content a span");
+            if (modelNameElem) {
+                modelName = modelNameElem.innerText;
+            }
+        }
+    });
+
+    // Check for Categories title and Extract tags from div with class 'gallery-info__item tags'
+    let tagsTitleElem = document.querySelector(".gallery-info__item.tags span.gallery-info__title");
+    if (tagsTitleElem && tagsTitleElem.innerText === "Categories:") {
+        let tagsElems = document.querySelectorAll(".gallery-info__item.tags .gallery-info__content a");
+        tagsElems.forEach(function (elem) {
+            tags.push(elem.innerText);
+        });
+    }
+
+    var data = {
+        "url": currentUrl,
+        "modelName": modelName,
+        "tags": tags,
+        "imageLinks": imageLinks
+    };
+
+    console.log(data);
+    addDataToList(currentUrl, data);
+})();
---
+++ images_gathering/morepics/morepics_download.py
@@ -0,0 +1,67 @@
+import argparse
+import concurrent.futures
+import json
+import os
+import requests
+from urllib.parse import urlparse
+import multiprocessing
+
+def download_image(image_url, target_folder, txt_content, counter):
+    url_path = urlparse(image_url).path
+    image_name = os.path.basename(url_path)
+    target_path = os.path.join(target_folder, image_name)
+
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    with open(target_path, 'wb') as f:
+        f.write(response.content)
+
+    txt_path = os.path.splitext(target_path)[0] + ".txt"
+    with open(txt_path, 'w') as f:
+        f.write(txt_content)
+
+    with counter.get_lock():
+        counter.value += 1
+        print(f"Downloaded {counter.value} images", end="\r")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", type=str, required=True)
+    parser.add_argument("--folder", type=str, required=True)
+    parser.add_argument("--additional-tags", type=str, required=True)
+    args = parser.parse_args()
+
+    with open(args.data, 'r') as f:
+        data = json.load(f)
+
+    os.makedirs(args.folder, exist_ok=True)
+
+    counter = multiprocessing.Value('i', 0)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+        for entry in data.values():
+            modelName = entry["modelName"].lower()
+            tags = ", ".join(tag.lower() for tag in entry["tags"])
+
+            additional_tags = args.additional_tags
+            # remove leading and trailing spaces, convert to lowercase, replace "_" with space, and split into a list
+            tag_list = [tag.strip().lower().replace("_", " ") for tag in additional_tags.split(",")]
+
+            if tag_list:
+                # convert list back to string
+                tag_string_cleaned = ", ".join(tag_list)
+                tag_string_cleaned = ", " + tag_string_cleaned
+
+            txt_content = f"{modelName}, {tags}{tag_string_cleaned}"
+
+            for image_url in entry["imageLinks"]:
+                executor.submit(download_image, image_url, args.folder, txt_content, counter)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Usage example: python3 /home/alexds9/Documents/stable_diffusion/stable_diffusion_tools/images_gathering/morepics/morepics_download.py --data '/path/download_info.json'  --folder '/path/Dowloads' --additional-tags "text, English text, signature, watermark, site address"
