8a17de6ee31ab7368aac3faa4415b4c1a48aab53
alexds9 <alexds9@gmail.com>
2023-07-01 15:34:02 +0300

    1. crop jitter fix1 suggestion.

--- crop_jitter_unitest.py
+++ crop_jitter_unitest.py
@@ -53,12 +53,14 @@ def _percent_random_crop(image, crop_jitter=0.02):
 
 def _percent_random_crop_fix1(image, crop_jitter=0.02):
     width, height = image.size
-    max_crop_pixels = min(width, height) * crop_jitter
+    max_crop_pixels = int(min(width, height) * crop_jitter)
 
-    left_crop_pixels = random.uniform(0, max_crop_pixels)
-    right_crop_pixels = random.uniform(0, max_crop_pixels)
-    top_crop_pixels = random.uniform(0, max_crop_pixels)
-    bottom_crop_pixels = random.uniform(0, max_crop_pixels)
+    left_crop_pixels = int(round(random.uniform(0, max_crop_pixels)))
+    right_crop_pixels = int(round(random.uniform(0, max_crop_pixels)))
+    top_crop_pixels = int(round(random.uniform(0, max_crop_pixels)))
+    bottom_crop_pixels = int(round(random.uniform(0, max_crop_pixels)))
+
+    #print(f"{left_crop_pixels}, {right_crop_pixels}, {top_crop_pixels}, {bottom_crop_pixels}, ")
 
     left = left_crop_pixels
     right = width - right_crop_pixels
@@ -68,49 +70,15 @@ def _percent_random_crop_fix1(image, crop_jitter=0.02):
     with patch.object(Image.Image, "crop", new=mock_crop):
         crop_size = image.crop((left, top, right, bottom))
 
-    cropped_width = width - int(left_crop_pixels + right_crop_pixels)
-    cropped_height = height - int(top_crop_pixels + bottom_crop_pixels)
+    cropped_width = int(width - (left_crop_pixels + right_crop_pixels))
+    cropped_height = int(height - (top_crop_pixels + bottom_crop_pixels))
 
-    cropped_aspect_ratio = cropped_width / cropped_height
-
-    if cropped_aspect_ratio > 1:
-        new_width = cropped_width
-        new_height = int(cropped_width / cropped_aspect_ratio)
-    else:
-        new_width = int(cropped_height * cropped_aspect_ratio)
-        new_height = cropped_height
 
     with patch.object(Image.Image, "resize", new=mock_resize):
-        resized_dims = image.resize((new_width, new_height))
+        resized_dims = image.resize((cropped_width, cropped_height))
 
     equal_dims = resized_dims == crop_size
     return crop_size, resized_dims, equal_dims
-
-
-# if __name__ == "__main__":
-#     parser = ArgumentParser()
-#     parser.add_argument("-f", "--folder", dest="folder", help="Path to the folder containing images.", required=True)
-#     args = parser.parse_args()
-#
-#     total_images = 0
-#     mismatch_count = 0
-#     for root, dirs, files in os.walk(args.folder):
-#         for file in files:
-#             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
-#                 total_images += 1
-#                 image_path = os.path.join(root, file)
-#                 image = Image.open(image_path)
-#                 crop_size, resized_dims, equal_dims = _percent_random_crop(image)
-#                 if not equal_dims:
-#                     mismatch_count += 1
-#                 diff_dims = tuple(a_i - b_i for a_i, b_i in zip(resized_dims, crop_size))
-#                 color_code = "\033[32m" if equal_dims else "\033[31m"
-#                 reset_code = "\033[0m"
-#                 filename = os.path.basename(image_path)
-#                 print(f"{color_code}{filename}{reset_code} - Image Size: {image.size}, Crop Box Size: {crop_size}, Resized Dimensions: {resized_dims}, Difference: {diff_dims}")
-#
-#     print(f"\nTotal images processed: {total_images}")
-#     print(f"Number of images with size mismatch: {mismatch_count}")
 
 
 def test_random_crop(image_path, mode):
@@ -118,6 +86,7 @@ def test_random_crop(image_path, mode):
     crop_jitter = 0.02
 
     image = Image.open(image_path)
+
 
     if mode == "original":
         crop_size, resized_dims, equal_dims = _percent_random_crop(image, crop_jitter)
@@ -131,8 +100,9 @@ def test_random_crop(image_path, mode):
     color_code = "\033[32m" if equal_dims else "\033[31m"
     reset_code = "\033[0m"
     filename = os.path.basename(image_path)
-    print(
-        f"{color_code}{filename}{reset_code} - Image Size: {image.size}, Crop Box Size: {crop_size}, Resized Dimensions: {resized_dims}, Difference: {diff_dims}")
+    if False == equal_dims:
+        print(
+            f"{color_code}{filename}{reset_code} - Image Size: {image.size}, Crop Box Size: {crop_size}, Resized Dimensions: {resized_dims}, Difference: {diff_dims}")
 
     return not equal_dims
 
